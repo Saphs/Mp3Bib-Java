e9d500ea93658828371ad40991dc7f0715e09641
Tizian Rettig <s4iltissy@web.de>
2017-11-09 23:01:39 +0100

    added gradle build config statements for testing dependencies

---
+++ src/test/java/MyFirstTestClass.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Tizian Rettig - Saphs
+ * @version 1.0.0
+ */
+public class MyFirstTestClass {
+
+    @Test
+    public void example(){
+        System.out.println("Hey there I am a test!");
+    }
+}
